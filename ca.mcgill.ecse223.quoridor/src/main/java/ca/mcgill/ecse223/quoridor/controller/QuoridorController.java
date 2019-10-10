f124aaf6126f4960717720e62104f6a9c99cd8c5
Alexander Legouverneur <alex.legouve@gmail.com>
2019-10-10 18:58:59 -0400

    Started Controller and Gherkin Scenarios for validate position.

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -49,6 +49,7 @@ public class QuoridorController {
 		throw new UnsupportedOperationException();
 	}
 
+
 	/**
 	 * Modifier method used to set the name of the white player
 	 * 
@@ -176,5 +177,94 @@ public class QuoridorController {
 	}
 	
 	
+
+
+	
+	/**
+	 * Method that initializes the Validation of the position
+	 * @param PawnPosition
+	 * @throws UnsupportedOperationException
+	 * @author Alexander Legouverneur
+	 */
+	public static boolean InitializeValidatePosition(int row, int col) throws UnsupportedOperationException {
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Method that validates the new pawn position and returns it as its new position
+	 * @param newPos
+	 * @throws UnsupportedOperationException
+	 * @author Alexander Legouverneur
+	 */
+	public static String ValidatePawnPosition(int row, int col) throws UnsupportedOperationException {
+		throw new UnsupportedOperationException();
+	}
+	/**
+	 * Method that initiates the validation of the position
+	 * @param wallPos
+	 * @param Walldir
+	 * @return true/false true for initialized validation
+	 * @throws UnsupportedOperationException
+	 * @author Alexander Legouverneur
+	 */
+	public static boolean InitiatePosValidation(Tile wallPos, Direction Walldir) throws UnsupportedOperationException{
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Method that returns the direction of the wall after validation
+	 * @param aWall
+	 * @return direction -of the wall
+	 * @throws UnsupportedOperationException
+	 * @author Alexander Legouverneur
+	 */
+	public static Direction ValidateWallDirection(Wall aWall ) throws UnsupportedOperationException{
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Returns the Tile of reference of the wall after the position is validated
+	 * @param aWall
+	 * @return Tile -where the wall starts
+	 * @throws UnsupportedOperationException
+	 */
+	public static Tile ValidateWallTile(int row, int col ) throws UnsupportedOperationException{
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * This method verifies if the given wall exists
+	 * @param wallPos
+	 * @param Walldir
+	 * @return true/false -true for existing
+	 * @throws UnsupportedOperationException
+	 * @author Alexander Legouverneur
+	 */
+	public static boolean CheckWallExist(Tile wallPos, Direction Walldir) throws UnsupportedOperationException{
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Method that checks if wall Position is valid
+	 * @param aWall
+	 * @return true/false
+	 * @throws UnsupportedOperationException
+	 * @author Alexander Legouverneur
+	 */
+	public static boolean CheckWallValid(Wall aWall) throws UnsupportedOperationException{
+		throw new UnsupportedOperationException();
+	}
+	
+//end of validate position
+	/**
+	 * Checks if the wall is on the side edge of the board
+	 * @param aWall
+	 * @return true/false -true if on side edge
+	 * @throws UnsupportedOperationException
+	 * @author Alexander Legouverneur
+	 */
+	public static boolean CheckWallSideEdge(Wall aWall) throws UnsupportedOperationException{
+		throw new UnsupportedOperationException();
+	}
 
 }
