8bfb94de39b3f0e77b7deb1fe6c5605100fe737c
tbutch <tristan.bouchard@mail.mcgill.ca>
2019-10-10 11:58:13 -0400

    Add javadocs to helper methods

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/HelperMethods.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/HelperMethods.java
@@ -5,8 +5,7 @@ import static org.junit.Assert.fail;
 import ca.mcgill.ecse223.quoridor.model.User;
 
 class HelperMethods {
-	
-	
+
 	// ***************************************
 	// Constants
 	public static final int TOTAL_NUMBER_OF_TILES = 81;
@@ -15,21 +14,30 @@ class HelperMethods {
 	public static final int WHITE_TILE_INDEX = 36;
 	public static final int BLACK_TILE_INDEX = 44;
 	// ***************************************
-	
+
+	/**
+	 * Helper method, used to validate if a user with a username is valid
+	 * 
+	 * @param user
+	 * @author Tristan Bouchard
+	 */
 	public static void validateUser(User user) {
-		if(user.equals(null)) {
+		if (user.equals(null)) {
 			fail();
 		}
 		if (!isUserNameValid(user.getName())) {
 			fail();
 		}
 	}
+
 	/**
 	 * Method to verify the validity of a selectedUsername.
+	 * 
 	 * @param userName - Name to verify
+	 * @author Tristan Bouchard
 	 */
 	public static Boolean isUserNameValid(String userName) {
 		userName = userName.trim();
-		return (userName != null && !userName.isEmpty() && !userName.equals("") );
+		return (userName != null && !userName.isEmpty() && !userName.equals(""));
 	}
 }
