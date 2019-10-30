2c61421a004921c67a2888a3ba39703aca9e55e1
Alexander Legouverneur <alex.legouve@gmail.com>
2019-10-30 14:33:37 -0400

    Did the new package for view and connected it to the Application.

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/QuoridorPage.java
@@ -0,0 +1,48 @@
+package ca.mcgill.ecse223.quoridor.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class QuoridorPage {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					QuoridorPage window = new QuoridorPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public QuoridorPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1165, 693);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblItWorked = new JLabel("IT WORKED");
+		lblItWorked.setBounds(280, 95, 303, 36);
+		frame.getContentPane().add(lblItWorked);
+	}
+}
