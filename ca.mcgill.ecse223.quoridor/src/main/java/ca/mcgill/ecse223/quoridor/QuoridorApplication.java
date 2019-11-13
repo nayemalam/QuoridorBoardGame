504eb49eccbc73d2913d60e51e8c72f66e399416
tbutch <tristan.bouchard@mail.mcgill.ca>
2019-11-13 14:51:34 -0500

    Merge iter4 branch into Nicolas-Dev + rebase

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/QuoridorApplication.java
@@ -2,7 +2,8 @@ package ca.mcgill.ecse223.quoridor;
 
 import ca.mcgill.ecse223.quoridor.model.Quoridor;
 
-
+public class QuoridorApplication{
+	
 	private static Quoridor quoridor;
 
 	public static Quoridor getQuoridor() {
