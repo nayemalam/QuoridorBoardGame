b6cb330b7eda6c427b8d118200a381f0c940fbab
Nicolas Buisson <nicolas.buisson@mail.mcgill.ca>
2019-11-24 18:18:11 -0500

    completed initial version of LoadGame and SaveGame controller methods

---
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/LoadGameStepDef.java
@@ -0,0 +1,48 @@
+package ca.mcgill.ecse223.quoridor.features;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoadGameStepDef {
+	@When("I initiate to load a game in {string}")
+	public void i_initiate_to_load_a_game_in(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("Each game move is valid")
+	public void each_game_move_is_valid() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("The game has no final results")
+	public void the_game_has_no_final_results() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("The game has a final result")
+	public void the_game_has_a_final_result() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("The game to load has an invalid move")
+	public void the_game_to_load_has_an_invalid_move() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@Then("The game shall notify the user that the game file is invalid")
+	public void the_game_shall_notify_the_user_that_the_game_file_is_invalid() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@Then("The game shall be in replay mode")
+	public void the_game_shall_be_in_replay_mode() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+}
