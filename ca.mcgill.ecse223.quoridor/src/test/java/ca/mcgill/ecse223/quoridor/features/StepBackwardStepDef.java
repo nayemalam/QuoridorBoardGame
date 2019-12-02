3e0a1c8ceb04031346c45a36fcabd1e2bc4d5b3c
Ousmane BARICISSE <ousmane.baricisse@mail.mcgill.ca>
2019-12-02 02:53:14 -0500

    Update StepBackwardStepDef.java

--- ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/StepBackwardStepDef.java
+++ ca.mcgill.ecse223.quoridor/src/test/java/ca/mcgill/ecse223/quoridor/features/StepBackwardStepDef.java
@@ -2,6 +2,7 @@ package ca.mcgill.ecse223.quoridor.features;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.List;
@@ -29,12 +30,12 @@ public class StepBackwardStepDef {
 	private int roundNumber;
 
 	@When("Step backward is initiated")
-	public void initStepForward() {
-
+	public void initStepForward() throws Exception {
+		QuoridorController.stepBackward();
 	}
 
 	@Then("White has <wwallno> on stock")
-	public void whiteHasWWallnoOnStock() {
+	public void whiteHasWWallnoOnStock(int wwall) {
 		// Write code here that turns the phrase above into concrete actions
 		
 	}
